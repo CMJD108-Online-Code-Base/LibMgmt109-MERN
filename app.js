62ea22b3d84fb4e4255be64d8360149609d11b11
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-15 13:54:39 +0530

    Create basic server use NodeJS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const PORT = 3500
 
+
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/plain"})
     res.end("Hello MEARN for CMJD - 108")
