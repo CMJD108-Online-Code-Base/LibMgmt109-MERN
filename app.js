02ae7bdb72500669f7e0ecaf4bd5e52944357037
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-15 15:05:32 +0530

    Create routings and other configs and optimized

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-// const http = require("http")
-// const PORT = 3500
-
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200,{"Content-Type":"text/plain"})
-//     res.end("Hello MEARN for CMJD - 108")
-// });
-
-// server.listen(PORT, ()=>{
-//     console.log(`Server started at the PORT: ${PORT}`)
-// })
-
-
 
 const express = require("express")
 const app = express()
-const PORT = 3600
+const PORT = 3700
+const bookRoutes = require("./routes/BookRoute")
 
-app.get("/",(req,res)=>{
-    res.send("Hello CMJD - 108")
-})
+
+app.use("/api/v1",bookRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`App Listening to: ${PORT}`)
