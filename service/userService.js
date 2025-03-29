ec413183619eb7cb40c0c8c12555ad9add32289c
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-29 19:40:14 +0530

    Create user service

---
+++ service/userService.js
@@ -0,0 +1,24 @@
+
+const User = require('../model/userModel')
+const bcrypt = require('bcryptjs')
+
+async function addUser(user){
+    try{
+        const hashPassword = await bcrypt.hash(user.password, 10)
+
+        const newUser = new User({
+            userId: user.userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: hashPassword,
+            role:user.role
+        });
+        return newUser.save();
+
+    }catch(err){
+        console.error(err);
+        throw err;
+    }
+}
+module.exports = { addUser }
