fd27b5bfe7d0f3fabc61f8a9da5bc9e2c16fcb9d
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-29 15:57:52 +0530

    Add MongoDB and mongoose

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const express = require("express")
 const app = express()
 const PORT = 3700
 const bookRoutes = require("./routes/BookRoute")
+const memberRoutes = require("./routes/MemberRoute")
+const mongoose = require("mongoose");
 
 
-app.use("/api/v1",bookRoutes)
+app.use("/api/v1",memberRoutes)
+
+//DB Integrate
+
+mongoose.connect("mongodb://localhost:27017/bookLib109",{useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("Failed to connect to MongoDB",err))
+
+
 
 app.listen(PORT, ()=>{
     console.log(`App Listening to: ${PORT}`)
